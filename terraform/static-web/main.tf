d8aa73f54fc5265400a648bb986d054d6fb4a75e
0xsudo <kaokakelvin@gmail.com>
2022-09-04 11:44:57 +0300

    first commit to ngix project

---
+++ terraform/static-web/main.tf
@@ -0,0 +1,33 @@
+terraform {
+   backend "s3" {
+     bucket         = "kaokakelvin-nginx"
+     key            = "terraform/static-web/terraform.tfstate"
+     region         = "us-east-1"
+     dynamodb_table = "terraform-state-locking-nginx"
+     encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.region
+    profile = var.profile
+}
+
+resource "aws_instance" "static-web" {
+    ami = "ami-052efd3df9dad4825"
+    instance_type = "t2.micro"
+    key_name = "nginx-keypair.pem"
+    vpc_security_group_ids = ["sg-08ff285b264c49053", "sg-0dc6164b1fff25c7f"]
+
+    tags = {
+        Name = var.name
+        Group = var.group
+    }
+}
---
+++ terraform/static-web/variables.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+    description = "Profile used to connect to AWS"
+}
+
+variable "region" {
+    description = "Region of resources deployment"
+}
+
+variable "group" {
+    description = "Used by ansible to group hosts"
+}
+
+variable "name" {
+    description = "Name of instance being launched"
+}
