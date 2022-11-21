cbf8fde423a289569d3d66451630d31caf7def88
0xsudo <kaokaonchangwakelvin@gmail.com>
2022-11-21 19:14:23 +0300

    changed provider versions and configurations

--- terraform/k8s/providers.tf
+++ terraform/k8s/providers.tf
@@ -1,24 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      version = "~> 4.0"
+      version = ">= 4.40.0"
     }
     helm = {
-      version = "~> 2.0"
+      version = ">= 2.7.1"
     }
     kubernetes = {
-      version = "~> 2.0"
+      version = ">= 2.16.0"
     }
   }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.prod-cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.prod-cluster.certificate_authority.0.data)
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.prod-cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
       command     = "aws"
     }
   }
@@ -26,10 +26,14 @@ provider "helm" {
 
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.prod-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.prod-cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.prod-cluster.token
-  #load_config_file       = false
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                   = data.aws_eks_cluster_auth.eks.token
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  #   command     = "aws"
+  # }
 }
 
 provider "aws" {
